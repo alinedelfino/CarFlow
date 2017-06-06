c13f23c2b4a007e06059004368cfcfe183700f0e
Aline Delfino Reis <aline.delfino@gmail.com>
2017-06-06 23:53:55 +0100

    Finally some statistics from rating!

--- CarFlow/Controllers/SalesAdvisorsController.cs
+++ CarFlow/Controllers/SalesAdvisorsController.cs
@@ -1,11 +1,9 @@
+using CarFlow.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using CarFlow.Models;
 
 namespace CarFlow.Controllers
 {
@@ -19,9 +17,54 @@ namespace CarFlow.Controllers
         }
 
         // GET: SalesAdvisors
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string name)
         {
-            return View(await _context.SalesAdvisor.ToListAsync());
+            // Use LINQ to get list of rates.            
+            var salesAdvisor = from m in _context.SalesAdvisor
+                         select m;
+
+            var salesOrder = from m in _context.SalesOrder
+                               select m;
+
+            // Get the ID of the sales Advisor
+            var ratingAverage = 0.0;
+            var numSales = 0;
+            if (!String.IsNullOrEmpty(name))
+            {
+                salesAdvisor = salesAdvisor.Where(x => x.Name.Contains(name));
+
+                // Now get all the sales orders from this particular saleAdvisor
+                //var salesAdvisorID = salesAdvisor.ElementAt(0).ID;
+                var salesAdvisorID = salesAdvisor.First().ID;
+                salesOrder = salesOrder.Where(x => x.SalesAdvisorId == salesAdvisorID);
+
+                // Get number of sales
+                numSales = salesOrder.Count();
+
+                // If there was any sail get the average rating
+                if (salesOrder.Any()) {              
+                    var sales = salesOrder.ToList();
+
+                    // Probably not the best LINQ way...              
+                    foreach (var element in sales)
+                    {
+                        var survey = from m in _context.Survey
+                                     select m;
+                        survey = survey.Where(x => x.SalesOrderId == element.ID);
+                        ratingAverage += survey.First().Rate;
+                    }
+                    ratingAverage /= sales.Count();                                        
+                }                
+
+            }
+
+            // Populate model
+            var salesAdvisorInfo = new SalesAdvisorRateInfo();
+            salesAdvisorInfo.avgRating = ratingAverage;
+            salesAdvisorInfo.numSales = numSales;
+            salesAdvisorInfo.salesAdvisor = await _context.SalesAdvisor.ToListAsync();
+
+            return View(salesAdvisorInfo);
         }
 
         // GET: SalesAdvisors/Details/5
---
+++ CarFlow/Models/SalesAdvisorRateInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarFlow.Models
+{
+    public class SalesAdvisorRateInfo
+    {
+        public List<SalesAdvisor> salesAdvisor;
+        public double avgRating { get; set; }
+        public int numSales { get; set; }
+    }
+}
